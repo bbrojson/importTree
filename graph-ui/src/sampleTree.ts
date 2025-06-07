f01959567209d76f9c8ec660f197da103077a81a
bbrojson <blazej.brojson@gmail.com>
2025-06-07 20:35:45 +0200

    test: adjust

--- graph-ui/src/sampleTree.ts
+++ graph-ui/src/sampleTree.ts
@@ -7,45 +7,45 @@ export function createSampleTree(): Tree<TreeNodeType> {
 
   // Root node
   const root = new TreeNode<TreeNodeType>({
-    file: "root.ts",
+    file: "root",
     variable: null,
   });
   tree.root = root;
 
   // Level 1 - 3 nodes
   const level1_1 = new TreeNode<TreeNodeType>({
-    file: "module1.ts",
+    file: "module1",
     variable: null,
   });
   const level1_2 = new TreeNode<TreeNodeType>({
-    file: "module2.ts",
+    file: "module2",
     variable: null,
   });
   const level1_3 = new TreeNode<TreeNodeType>({
-    file: "module3.ts",
+    file: "module3",
     variable: null,
   });
   root.children = [level1_1, level1_2, level1_3];
 
   // Level 2 - 5 nodes
   const level2_1 = new TreeNode<TreeNodeType>({
-    file: "submodule1.ts",
+    file: "sub1",
     variable: null,
   });
   const level2_3 = new TreeNode<TreeNodeType>({
-    file: "submodule3.ts",
+    file: "sub3",
     variable: null,
   });
   const level2_4 = new TreeNode<TreeNodeType>({
-    file: "submodule4.ts",
+    file: "sub4",
     variable: null,
   });
   const level2_5 = new TreeNode<TreeNodeType>({
-    file: "submodule5.ts",
+    file: "sub5",
     variable: null,
   });
   const level2_6 = new TreeNode<TreeNodeType>({
-    file: "submodule6.ts",
+    file: "sub6",
     variable: null,
   });
 
@@ -55,53 +55,53 @@ export function createSampleTree(): Tree<TreeNodeType> {
 
   // Level 3 - 10 nodes
   const level3_1 = new TreeNode<TreeNodeType>({
-    file: "component1.ts",
+    file: "component1",
     variable: null,
   });
   const level3_2 = new TreeNode<TreeNodeType>({
-    file: "component2.ts",
+    file: "component2",
     variable: null,
   });
   const level3_4 = new TreeNode<TreeNodeType>({
-    file: "component4.ts",
+    file: "component4",
     variable: null,
   });
   const level3_5 = new TreeNode<TreeNodeType>({
-    file: "component5.ts",
+    file: "component5",
     variable: null,
   });
   const level3_6 = new TreeNode<TreeNodeType>({
-    file: "component6.ts",
+    file: "component6",
     variable: null,
   });
   const level3_7 = new TreeNode<TreeNodeType>({
-    file: "component7.ts",
+    file: "component7",
     variable: null,
   });
   const level3_8 = new TreeNode<TreeNodeType>({
-    file: "component8.ts",
+    file: "component8",
     variable: null,
   });
   const level3_9 = new TreeNode<TreeNodeType>({
-    file: "component9.ts",
+    file: "component9",
     variable: null,
   });
   const level3_10 = new TreeNode<TreeNodeType>({
-    file: "component10.ts",
+    file: "component10",
     variable: null,
   });
   const level3_11 = new TreeNode<TreeNodeType>({
-    file: "component11.ts",
+    file: "component11",
     variable: null,
   });
 
   // Base nodes
   const baseA = new TreeNode<TreeNodeType>({
-    file: "baseA.ts",
+    file: "baseA",
     variable: null,
   });
   const baseB = new TreeNode<TreeNodeType>({
-    file: "baseB.ts",
+    file: "baseB",
     variable: null,
   });
 
