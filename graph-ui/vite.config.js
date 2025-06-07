f72d899537a6993a26acb5d878739ec04a0b8d0e
bbrojson <blazej.brojson@gmail.com>
2025-06-07 15:13:09 +0200

    test: adjust

--- graph-ui/vite.config.js
+++ graph-ui/vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "out",
+    emptyOutDir: true,
+  },
 });
 //# sourceMappingURL=vite.config.js.map
